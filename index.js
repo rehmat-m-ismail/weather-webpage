05312338fa0ac6f0404f3adccd8114276a0615b0
rehmat-m-ismail <rehmatmuhammadismail@gmail.com>
2024-10-06 18:55:53 +0500

    weather-page

---
+++ index.js
@@ -0,0 +1,8 @@
+let weather=["karachi","lahore","peshawar","islambad","sindh","punjab","multan","gujrat","DIG","muree"];
+console.log(weather);
+let userinput =prompt("Enter your city Name");
+if(userinput===weather){
+    console.log("thank you")
+    alert("thank u")
+}
+
